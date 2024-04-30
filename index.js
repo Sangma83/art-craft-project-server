4833f16c234b50cd549ddf3242fd93d90e3ea173
Sangma83 <poushalisangma13@gmail.com>
2024-05-01 02:08:13 +0600

    uri set and mongodb connect done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 
@@ -12,6 +12,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//ArtCraftItems
+//eT2RJrRdevCHAMRn
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4xueldm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -135,3 +137,39 @@ app.get('/', (req, res) =>{
 app.listen(port, () =>{
     console.log(`Coffee server is running on : ${port}`)
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+
