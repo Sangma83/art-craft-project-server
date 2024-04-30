0922b4c19eb0602518f1668a2b7aa206fa9764b4
Sangma83 <poushalisangma13@gmail.com>
2024-05-01 05:22:33 +0600

    craft added successfully

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,8 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//ArtCraftItems
-//eT2RJrRdevCHAMRn
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4xueldm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -34,90 +31,90 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
 
 
-   const coffeeCollection = client.db('coffeeDB').collection('coffee');
-   const userCollection = client.db('coffeeDB').collection('user');
+   const craftCollection = client.db('craftDB').collection('craft');
+  //  const userCollection = client.db('coffeeDB').collection('user');
    
 
-  app.get('/coffee', async(req, res) =>{
-    const cursor = coffeeCollection.find();
+  app.get('/crafts', async(req, res) =>{
+    const cursor = craftCollection.find();
     const result = await cursor.toArray(cursor);
     res.send(result);
   })
  
-  app.get('/coffee/:id', async(req, res) =>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await coffeeCollection.findOne(query);
-    res.send(result);
-  })
+  // app.get('/coffee/:id', async(req, res) =>{
+  //   const id = req.params.id;
+  //   const query = {_id: new ObjectId(id)}
+  //   const result = await coffeeCollection.findOne(query);
+  //   res.send(result);
+  // })
   
-  app.post('/coffee', async(req, res) =>{
-  const newCoffee = req.body;
-  console.log(newCoffee);
-  const result = await coffeeCollection.insertOne(newCoffee);
+  app.post('/crafts', async(req, res) =>{
+  const newCraft = req.body;
+  console.log(newCraft);
+  const result = await craftCollection.insertOne(newCraft);
   res.send(result)
   })
 
-  app.put('/coffee/:id', async(req, res) =>{
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)};
-    const options = {upsert: true};
-    const updatedCoffee = req.body;
-    const coffee = {
-      $set: {
-        name: updatedCoffee.name, 
-        quantity: updatedCoffee.quantity,
-        supplier: updatedCoffee.supplier,
-        taste: updatedCoffee.taste,
-        category: updatedCoffee.category,
-        details: updatedCoffee.details,
-        photo: updatedCoffee.photo
-      }
-    }
-    const result = await coffeeCollection.updateOne(filter, coffee, options);
-    res.send(result);
-  })
+  // app.put('/coffee/:id', async(req, res) =>{
+  //   const id = req.params.id;
+  //   const filter = {_id: new ObjectId(id)};
+  //   const options = {upsert: true};
+  //   const updatedCoffee = req.body;
+  //   const coffee = {
+  //     $set: {
+  //       name: updatedCoffee.name, 
+  //       quantity: updatedCoffee.quantity,
+  //       supplier: updatedCoffee.supplier,
+  //       taste: updatedCoffee.taste,
+  //       category: updatedCoffee.category,
+  //       details: updatedCoffee.details,
+  //       photo: updatedCoffee.photo
+  //     }
+  //   }
+  //   const result = await coffeeCollection.updateOne(filter, coffee, options);
+  //   res.send(result);
+  // })
 
-  app.delete('/coffee/:id', async(req, res) =>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)};
-    const result = await coffeeCollection.deleteOne(query);
-    res.send(result);
-  })
+  // app.delete('/coffee/:id', async(req, res) =>{
+  //   const id = req.params.id;
+  //   const query = {_id: new ObjectId(id)};
+  //   const result = await coffeeCollection.deleteOne(query);
+  //   res.send(result);
+  // })
 
-  //user related apis
+  // //user related apis
 
-  app.get('/user', async(req, res) =>{
-    const cursor = userCollection.find();
-    const users = await cursor.toArray();
-    res.send(users);
-  })
+  // app.get('/user', async(req, res) =>{
+  //   const cursor = userCollection.find();
+  //   const users = await cursor.toArray();
+  //   res.send(users);
+  // })
 
-  app.post('/user', async(req, res) =>{
-    const user = req.body;
-    console.log(user);
-    const result = await userCollection.insertOne(user);
-    res.send(result);
-  })
+  // app.post('/user', async(req, res) =>{
+  //   const user = req.body;
+  //   console.log(user);
+  //   const result = await userCollection.insertOne(user);
+  //   res.send(result);
+  // })
 
-  app.patch('/user', async(req, res) =>{
-    const user = req.body;
-    const filter = { email: user.email};
-    const updatedDoc = {
-      $set: {
-        lastLoggedAt: user.lastLoggedAt
-      }
-    }
-    const result = await userCollection.updateOne(filter, updatedDoc);
-    res.send(result);
-  })
+  // app.patch('/user', async(req, res) =>{
+  //   const user = req.body;
+  //   const filter = { email: user.email};
+  //   const updatedDoc = {
+  //     $set: {
+  //       lastLoggedAt: user.lastLoggedAt
+  //     }
+  //   }
+  //   const result = await userCollection.updateOne(filter, updatedDoc);
+  //   res.send(result);
+  // })
 
-  app.delete('/user/:id', async(req, res) =>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)};
-    const result = await userCollection.deleteOne(query);
-    res.send(result);
-  })
+  // app.delete('/user/:id', async(req, res) =>{
+  //   const id = req.params.id;
+  //   const query = {_id: new ObjectId(id)};
+  //   const result = await userCollection.deleteOne(query);
+  //   res.send(result);
+  // })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
@@ -131,11 +128,11 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) =>{
-    res.send('Coffee server is running')
+    res.send('Art & Craft server is running')
 })
 
 app.listen(port, () =>{
-    console.log(`Coffee server is running on : ${port}`)
+    console.log(`Art & Craft server is running on : ${port}`)
 })
 
 
