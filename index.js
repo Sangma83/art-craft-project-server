735361ac2c1397d64f829d833f8ee72ca964aa2b
Sangma83 <poushalisangma13@gmail.com>
2024-05-03 15:43:41 +0600

    my list delete function added

--- index.js
+++ index.js
@@ -76,12 +76,12 @@ async function run() {
   //   res.send(result);
   // })
 
-  // app.delete('/crafts/:id', async(req, res) =>{
-  //   const id = req.params.id;
-  //   const query = {_id: new ObjectId(id)};
-  //   const result = await craftCollection.deleteOne(query);
-  //   res.send(result);
-  // })
+  app.delete('/crafts/:id', async(req, res) =>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await craftCollection.deleteOne(query);
+    res.send(result);
+  })
 
   // //user related apis
 
